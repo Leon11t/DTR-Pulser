cac228d80700a3076ed1b34e0c54df0aad612368
Leon11t <malui2@yandex.ru>
2014-01-26 12:43:58 +0200

    First relise

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSavePort = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnPuls = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // portBox
+            // 
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(12, 12);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(63, 21);
+            this.portBox.TabIndex = 0;
+            this.portBox.SelectedIndexChanged += new System.EventHandler(this.portBox_SelectedIndexChanged);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnSavePort
+            // 
+            this.btnSavePort.Location = new System.Drawing.Point(81, 10);
+            this.btnSavePort.Name = "btnSavePort";
+            this.btnSavePort.Size = new System.Drawing.Size(28, 23);
+            this.btnSavePort.TabIndex = 2;
+            this.btnSavePort.Text = "<";
+            this.btnSavePort.UseVisualStyleBackColor = true;
+            this.btnSavePort.Click += new System.EventHandler(this.btnSavePort_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(99, 45);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(58, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "digiua.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // btnPuls
+            // 
+            this.btnPuls.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPuls.Location = new System.Drawing.Point(115, 10);
+            this.btnPuls.Name = "btnPuls";
+            this.btnPuls.Size = new System.Drawing.Size(34, 23);
+            this.btnPuls.TabIndex = 4;
+            this.btnPuls.Text = "P";
+            this.btnPuls.UseVisualStyleBackColor = true;
+            this.btnPuls.Click += new System.EventHandler(this.btnPuls_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(156, 70);
+            this.Controls.Add(this.btnPuls);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnSavePort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.portBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DTR Pulser";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox portBox;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSavePort;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button btnPuls;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)                                 //Ініціалізація основної форми                      
+        {
+            foreach (string item in System.IO.Ports.SerialPort.GetPortNames())              //Взнаємо які ком-порти є в системі
+            {
+                portBox.Items.Add(item);                                                    //Додаєм їх в ліст-бокс
+                portBox.SelectedIndex = 0;
+            }
+
+            try
+            {                                                                               //Перевіряєм наявність портів
+                label1.Text = "OPEN";
+                portBox.Text = Properties.Settings.Default.PortName;                        //Пізтаємо збережені параметри із сейву
+                label1.Text = "READ PORT";
+                serialPort1.Open();
+                label1.Text = "OPEN PORT";
+                serialPort1.DtrEnable = true;
+                label1.Text = "SEND DTR";
+                serialPort1.Close();
+                label1.Text = "DONE!";
+
+            }
+            catch (Exception)
+            {                                                                               //Якщо нема ніодного 
+                label1.Text = "OPEN FAIL";                                                  //То вишемо це в лейблі
+            }
+
+                timer1.Start();                                                             //Запускаємо маймер для закриття проги
+
+        }
+
+        private void portBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            serialPort1.PortName = portBox.Text;                                             //Вибираєм ВАШ КОМ-порт
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();                                                               //По тіку таймера закриваємо прогу
+        }
+
+        private void btnSavePort_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();                                                                    //Зупиняєм таймер якщо нажата кнопка параметрів
+            Properties.Settings.Default.PortName = portBox.Text;                              
+            Properties.Settings.Default.Save();                                               //Зберігаємо параметри
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            {
+                Process.Start("http://digiua.com/");                                           //Лінк на сайт
+            }
+        }
+
+        private void btnPuls_Click(object sender, EventArgs e)                                  //Кнопка "Р"
+        {
+            timer1.Stop();
+            try
+            {
+                serialPort1.Open();
+                label1.Text = "OPEN PORT";
+                serialPort1.DtrEnable = true;
+                label1.Text = "SEND DTR";
+            }
+            catch (Exception)
+            {
+
+                label1.Text = "OPEN FAIL";
+            }
+            
+        }
+        
+      
+
+    }
+}
